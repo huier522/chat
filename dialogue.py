dd7829f4ecf2cbf64e89d92230d7c2f9fd10750f
huier <andychen522.ambit@gmail.com>
2021-06-06 01:18:33 +0800

    add convert function

--- dialogue.py
+++ dialogue.py
@@ -6,9 +6,25 @@ def read_file(filename):
             chat.append(line.strip())
         return chat
 
+def convert(chat):
+    new_chat = []
+    person = None #先給空值，避免一開始不是人名（空值）時程式出錯
+    for line in chat:
+        if line == 'Allen':
+            person = 'Allen'
+            continue #跳過只有人名卻沒有對話時裝入清單
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person: #若person是空值
+            new_chat.append(person + ': ' + line)
+    return new_chat
+
 def main():
     filename = 'input.txt'
     dialogue = read_file(filename)
+    print(dialogue)
+    dialogue = convert(dialogue)
     print(dialogue)
 
 main()
